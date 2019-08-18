8d110c408b9ea5c89e74a736f6dcb90834bc2fe2
durwa004 <durwa004@umn.edu>
2019-08-18 15:42:45 -0500

    Modified to try out new script

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Mon Aug  5 11:04:17 2019
-
 @author: jonahcullen
 """
 
@@ -21,17 +20,24 @@ def make_arg_parser():
             required=True,
             help="Path to dir containing the ibio output files [required]")
     parser.add_argument(
-            "-v", "--vcf",
+            "-i", "--ids",
             default=argparse.SUPPRESS,
             metavar="",
             required=True,
-            help="Location of vcf file for analysis [required]")
+            help="Text file containing horse ids - one per line [required]")
     parser.add_argument(
-            "-v", "--vcf",
+            "-c", "--command",
             default=argparse.SUPPRESS,
             metavar="",
             required=True,
-            help="Location of vcf file for analysis [required]")
+            help="s3cmd command e.g. get [required]")
+    parser.add_argument(
+            "-ft", "--filetype",
+            default=argparse.SUPPRESS,
+            metavar="",
+            required=True,
+            help="file type to be extracted e.g. realigned.bam [required]")
+
     return parser
 
 
@@ -41,33 +47,32 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     data = os.path.abspath(args.data)
-    vcf_file = args.vcf
+    horse_ids = os.path.abspath(args.ids)
+    com = args.command
+    ft = args.filetype
 
-    gatk = "/home/mccuem/shared/.local/conda/envs/HorseGenomeProject/bin/gatk/gatk"
-    java = '--java-options "-Xmx4g"'
-    ref = "/home/mccuem/shared/Projects/HorseGenomeProject/Data/ibio_EquCab3/NCBI_reference/GCF_002863925.1_EquCab3.0_genomic.fna"
- 
-    print(f"Using {gatk}\nwith equine reference {ref}")
-
+    s3cmd = "/usr/bin/s3cmd  --config /home/mccuem/shared/.local/.s3cfg"
+    
     header = (
               "#!/bin/bash -l\n"  
-              "#PBS -l nodes=1:ppn=12,walltime=48:00:00,mem=12g\n"
+              "#PBS -l nodes=1:ppn=12,walltime=48:00:00,mem=4g\n"
               "#PBS -m abe\n"
               "#PBS -M durwa004@umn.edu\n"
-              f"#PBS -o $PBS_JOBID.gatk_intersect.out\n"
-              f"#PBS -e $PBS_JOBID.gatk_intersect.err\n"
-              f"#PBS -N gatk_intersect.pbs\n"
+              f"#PBS -o $PBS_JOBID.s3cmd_{com}.out\n"
+              f"#PBS -e $PBS_JOBID.s3cmd_{com}.err\n"
+              f"#PBS -N s3cmd_{com}.pbs\n"
               "#PBS -q small\n"
              )
     
-    pbs = os.path.join(os.getcwd(), f"gatk_intersect.pbs")
+    tmp = []
+    with open(horse_ids) as ids:
+        for line in ids:
+            tmp.append(line.strip())
+    
+    pbs = os.path.join(os.getcwd(),f"s3cmd_{com}.pbs")
     
     with open(pbs, "w") as f:
         print(header, file=f)
         print(f"cd {data}\n", file=f)
-        print(f"{gatk} {java}" 
-              + f" {prog} -R "
-              + f"{ref} \\"
-              + "  -V "
-              + f"{vcf_file} \\"
-              + f" -o ibio_all_intersect.vcf.gz -nt 4", file=f)
+        for i in tmp:
+            print(f"{s3cmd} {com} s3://mccue-lab/ibiodatatransfer2019/{i}/{i}.{ft}",file=f) 
